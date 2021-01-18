dfdda8ea749ac822039dfbb0122072a890055390
Leonardo Noya garcia <a2019115506@estudiantes.upsa.edu.bo>
2021-01-18 09:16:53 -0400

    suma en get y post

---
+++ Solucion_Operaciones/Web_Operaciones/Controllers/CalculadoraController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Operaciones.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        [Route("Suma")]
+        public int suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        [HttpPost]
+        [Route("Suma")]
+        public int sumar([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
